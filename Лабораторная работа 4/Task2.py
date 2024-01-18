16f30c94e6ce90635e3af7d6262017479c9712d8
Uggen <149669796+Uggen@users.noreply.github.com>
2024-01-18 21:05:13 +0300

    Add files via upload

---
+++ Лабораторная работа 4/Task2.py
@@ -0,0 +1,26 @@
+# TODO импортировать необходимые молули
+import csv
+import json
+
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+
+def task() -> None:
+    # TODO считать содержимое csv файла
+    with open(INPUT_FILENAME, encoding='utf-8') as f:
+        data = [line for line in csv.DictReader(f)]
+
+    # TODO Сериализовать в файл с отступами равными 4
+    with open(OUTPUT_FILENAME, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
+
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
